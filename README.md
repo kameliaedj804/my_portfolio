e3db3ac479853f8bec8d72321edc81f8c5f12154
Kamelia Edjekouane <k.edjekouane@lillybelle.eu>
2026-02-23 15:06:13 +0100

    Commit unique : état actuel du projet

---
+++ README.md
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+# portfolio
+=======
+# my_portfolio
+>>>>>>> fba02cd1a9f307956c8a6cc7d6717151c773bb12
---
+++ hamburger.js
@@ -0,0 +1,8 @@
+// Burger menu toggle
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  burger.classList.toggle('toggle');
+});
